bbebcbe11abfa04f8f873fa23e8be457f04fd3d6
Lyxal <36217120+Lyxal@users.noreply.github.com>
2023-11-08 22:31:28 +1100

    Update filter_files.py

--- filter_files.py
+++ filter_files.py
@@ -2,5 +2,5 @@ import sys
 import json
 
 if __name__ == "__main__":
-  files = sys.argv[1].split("\n")
+  files = [x for x in sys.argv[1].split("\n") if x.endswith("js") or x.endswith("map")]
   print(json.dumps(files))
